849092116a18ba665814577e8a09738fbfbac43c
John Ward <john@johnward.net>
2020-04-04 23:36:03 +0100

    Clear up 1

--- main.cpp
+++ main.cpp
@@ -6,9 +6,15 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <vector>
+#include "Mesh.h"
 
-GLuint VAO, VBO, IBO, shader, uniformModel;
+const GLint WIDTH = 800, HEIGHT = 600;
 const float toRadians = 3.14159265f / 180.0f;
+
+std::vector<Mesh *> meshList;
+
+GLuint shader, uniformModel, uniformProjection;
 
 bool direction = true;
 float triOffset = 0.0f;
@@ -29,13 +35,14 @@ static const char *vShader = "						\n\
 layout (location = 0) in vec3 pos;					\n\
                 									\n\
 out vec4 vCol;										\n\
-													\n\
-uniform mat4 model;									\n\
-            										\n\
-void main()											\n\
-{													\n\
-	gl_Position = model * vec4(pos, 1.0);			\n\
-	vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);		\n\
+																\n\
+uniform mat4 model;												\n\
+uniform mat4 projection;										\n\
+            													\n\
+void main()														\n\
+{																\n\
+	gl_Position = projection * model * vec4(pos, 1.0);			\n\
+	vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);					\n\
 }";
 
 // Fragment Shader
@@ -67,24 +74,9 @@ void CreateTriangle()
 		1.0f, -1.0f, 0.0f,	// Bottom Right
 		0.0f, 1.0f, 0.0f};	// Top
 
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-
-	glGenBuffers(1, &IBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(0);
-
-	// Unbind above
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
+	Mesh *obj1 = new Mesh();
+	obj1->CreateMesh(vertices, indices, 12, 12);
+	meshList.push_back(obj1);
 }
 
 void AddShader(GLuint theProgram, const char *shaderCode, GLenum shaderType)
@@ -150,10 +142,8 @@ void CompileShaders()
 	}
 
 	uniformModel = glGetUniformLocation(shader, "model");
+	uniformProjection = glGetUniformLocation(shader, "projection");
 }
-
-const GLint WIDTH = 800;
-const GLint HEIGHT = 600;
 
 int main()
 {
@@ -213,6 +203,8 @@ int main()
 	CreateTriangle();
 	CompileShaders();
 
+	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)bufferWidth / (GLfloat)bufferHeight, 0.1f, 100.0f);
+
 	// main loop until window closed
 
 	while (!glfwWindowShouldClose(mainWindow))
@@ -261,21 +253,16 @@ int main()
 		glUseProgram(shader);
 
 		glm::mat4 model(1.0f);
+		model = glm::translate(model, glm::vec3(0.0f, triOffset, -2.5f));
 		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
-		//model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+
 		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
 
 		// sets "model" in the shader
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, &model[0][0]);
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
 
-		glBindVertexArray(VAO);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
-
-		//glDrawArrays(GL_TRIANGLES, 0, 3);
-		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
-
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-		glBindVertexArray(0);
+		meshList[0]->RenderMesh();
 
 		glUseProgram(0);
 
